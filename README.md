a926d2121c64d1907c1f105a46fb24856da78505
Dopa91 <code.dopatka@gmail.com>
2024-12-04 16:54:46 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tasksheet_api_flutter_5_4_2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tasksheet_api_flutter_5_4_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasksheet_api_flutter_5_4_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
