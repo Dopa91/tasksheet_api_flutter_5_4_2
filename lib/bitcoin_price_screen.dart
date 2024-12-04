6a6291b74d2b0d463939983c57dd2a6768818b05
Dopa91 <code.dopatka@gmail.com>
2024-12-04 18:13:17 +0100

    intentionally added an error for presentation

--- lib/bitcoin_price_screen.dart
+++ lib/bitcoin_price_screen.dart
@@ -14,7 +14,7 @@ class BitcoinPriceScreenState extends State<BitcoinPriceScreen> {
 
   Future<void> fetchBitcoinPrice() async {
     const url =
-        'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
+        'https://apicoingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
